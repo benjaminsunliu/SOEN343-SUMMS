ed93f51cb0efdd0dc656855d45fc9093e114de53
Benjamin Liu <liubenjamin8@gmail.com>
2026-02-26 16:00:42 -0500

    initialize project base

---
+++ server/src/main/java/com/thehorselegend/summs/SummsApplication.java
@@ -0,0 +1,13 @@
+package com.thehorselegend.summs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/thehorselegend/summs/SummsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehorselegend.summs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
